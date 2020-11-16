d4d7697ba0a89fd142171b99aa3f9e13d62325ce
lisheng <lisheng@zhidaoauto.com>
2020-11-16 14:19:28 +0800

    add dodo

--- roxe.contracts/test/bios-boot-tutorial/mycmd.sh
+++ roxe.contracts/test/bios-boot-tutorial/mycmd.sh
@@ -28,7 +28,7 @@ $CLS set contract daimkrdaimkr /data/roxe/balanceos/RoxeChain/roxe.contracts/bui
 ;;
 "ss") 
 $CLS system newaccount roxe1 dai2mkr11111 ROXE6ftHab5c81LAcL1izHNyFVawBaZTEpFDXN3BYybx1pcJHQsTmH --stake-net "10000.0000 ROC" --stake-cpu "10000.0000 ROC" --buy-ram "10000.0000 ROC" -p roxe1@active
-$CLS set contract dai2mkr11111 /data/roxe/balanceos/RoxeChain/roxe.contracts/build/contracts/roxe.token -p daimkrdaimkr
+$CLS set contract dai2mkr11111 /data/roxe/balanceos/RoxeChain/roxe.contracts/build/contracts/roxe.token -p dai2mkr11111
 ;;
 "t") 
 $CLS transfer rox1 dai2mkr11111 "1000000.0000 ROC"
