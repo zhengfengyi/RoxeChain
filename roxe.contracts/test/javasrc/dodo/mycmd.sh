ba476841dee7bae1697e74e16a1a90d8f6f938a7
vlbos <vlbos2018@gmail.com>
2020-11-30 10:31:16 +0800

    pool record add Reference

--- roxe.contracts/test/javasrc/dodo/mycmd.sh
+++
@@ -1,7 +0,0 @@
-case "$1" in
-"c") javac TestApiDemo.java ;;
-"e") java TestApiDemo   ;;
-*) echo "c --compile e --execute" ;;
-esac
-  
-
--- roxe.contracts/test/javasrc/swap/mycmd.sh
+++
@@ -1,7 +0,0 @@
-case "$1" in
-"c") javac ./SwapApiDemo.java ;;
-"e") java SwapApiDemo   ;;
-*) echo "c --compile e --execute" ;;
-esac
-  
-
